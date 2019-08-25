d78766d4dfffe04690d15bb761648baf01e59f8a
Lois Chan-Pedley <lois@frontmatter.ca>
2019-08-24 17:36:23 -0700

    placed menu in mostly correct location though it's still looking really rough

--- header.php
+++ header.php
@@ -31,7 +31,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<header class="navbar utility bg-primary">	
+		<header class="navbar bg-primary">	
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
@@ -55,11 +55,51 @@ $container = get_theme_mod( 'understrap_container_type' );
 					the_custom_logo();
 				} ?><!-- end custom logo -->
 
-				<div class="pull-right">
+				<div class="mt-auto" id="nav-stuff">
 
-				  <div class="inline-form header-search">
-						<?php get_search_form(); ?>
-			    </div>
+					<div class="utility bg-primary row justify-content-end">
+					  <div class="inline-form header-search col-md-6">
+							<?php get_search_form(); ?>
+				    </div>
+
+				    <!-- custom language switcher -->
+						<div class="lang-switcher col-md-auto">
+							<?php do_action('wpml_add_language_selector'); ?>
+						</div>
+						<!-- end custom language switcher -->
+						
+				  </div>
+
+			    <nav class="navbar navbar-expand-lg navbar-dark bg-primary px-0">
+
+						<?php if ( 'container' == $container ) : ?>
+						<div class="container pr-0">
+						<?php endif; ?>
+
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+								<span class="navbar-toggler-icon"></span>
+							</button>
+
+
+							<!-- The WordPress Menu goes here -->
+							<?php wp_nav_menu(
+								array(
+									'theme_location'  => 'primary',
+									'container_class' => 'collapse navbar-collapse',
+									'container_id'    => 'navbarNavDropdown',
+									'menu_class'      => 'navbar-nav mr-auto',
+									'fallback_cb'     => '',
+									'menu_id'         => 'main-menu',
+									'depth'           => 2,
+									'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+								)
+							); ?>
+
+						<?php if ( 'container' == $container ) : ?>
+						</div><!-- .container -->
+						<?php endif; ?>
+
+					</nav><!-- .site-navigation -->
 				</div>
 
 			<?php if ( 'container' == $container ) : ?>
@@ -68,41 +108,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		</header>
 
-		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-
-			<?php if ( 'container' == $container ) : ?>
-			<div class="container">
-			<?php endif; ?>
-
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-
-				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav mr-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-
-		    <!-- custom language switcher -->
-				<div class="lang-switcher pull-right">
-					<?php do_action('wpml_add_language_selector'); ?>
-				</div>
-				<!-- end custom language switcher -->
-				
-			<?php if ( 'container' == $container ) : ?>
-			</div><!-- .container -->
-			<?php endif; ?>
-
-		</nav><!-- .site-navigation -->
+		
 
 	</div><!-- #wrapper-navbar end -->

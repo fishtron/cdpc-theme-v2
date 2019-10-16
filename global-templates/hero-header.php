78026d8fbfa0f63c18ac91b1a134ab6e5164a135
Lois Chan-Pedley <lois@frontmatter.ca>
2019-10-16 12:20:24 -0700

    made hero header taller

--- global-templates/hero-header.php
+++ global-templates/hero-header.php
@@ -14,8 +14,8 @@ $hero_title = get_field('hero_title') ? get_field('hero_title') : get_the_title(
 ?>
 
 <?php if ( get_field('hero_image') ) : ?>
-  <div class="hero-header jumbotron jumbotron-fluid <?php if ( get_field('dimmed') == true ): ?>overlay<?php endif ?>" style="background: url('<?php the_field('hero_image') ?>') no-repeat center center; min-height: 300px; background-size: cover;">
-    <div class="container py-3">
+  <div class="hero-header jumbotron jumbotron-fluid <?php if ( get_field('dimmed') == true ): ?>overlay<?php endif ?>" style="background: url('<?php the_field('hero_image') ?>') no-repeat center center; min-height: 500px; background-size: cover;">
+    <div class="container py-5">
       <h1 class="display-4"><?php echo $hero_title; ?></h1>
       <?php if ( get_field ('lead') ) : ?>
         <div class="lead display-5 my-3"><?php the_field('lead'); ?></div>

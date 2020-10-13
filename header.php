210ead1c723dcb240402e806695da5c95f20c77a
Lois Chan-Pedley <lois@frontmatter.ca>
2020-10-13 13:02:46 -0700

    Add Google Tag Manager code to enable pdf download tracking

--- header.php
+++ header.php
@@ -27,13 +27,25 @@ $container = get_theme_mod( 'understrap_container_type' );
 	  window.dataLayer = window.dataLayer || [];
 	  function gtag(){dataLayer.push(arguments);}
 	  gtag('js', new Date());
-
 	  gtag('config', 'UA-27324779-6');
 	</script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WJR846V');</script>
+	<!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJR846V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
 
